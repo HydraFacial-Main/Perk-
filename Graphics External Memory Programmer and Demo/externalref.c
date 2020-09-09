7bc68359e2f4e1925360750714421931642e282a
dhernandez-hydrafacial <68864688+dhernandez-hydrafacial@users.noreply.github.com>
2020-09-09 12:23:00 -0700

    Add files via upload

---
+++ Graphics External Memory Programmer and Demo/externalref.c
@@ -0,0 +1,97 @@
+/*****************************************************************************
+ * FileName:        external.c
+ * Processor:       PIC24F, PIC24H, dsPIC
+ * Compiler:        MPLAB C30 (see release notes for tested revision)
+ * Linker:          MPLAB LINK30
+ * Company:         Microchip Technology, Inc.
+ *
+ * Software License Agreement
+ *
+ * Copyright © 2010 Microchip Technology Inc.  All rights reserved.
+ * Microchip licenses to you the right to use, modify, copy and distribute
+ * Software only when embedded on a Microchip microcontroller or digital
+ * signal controller, which is integrated into your product or third party
+ * product (pursuant to the sublicense terms in the accompanying license
+ * agreement).
+ *
+ * You should refer to the license agreement accompanying this Software
+ * for additional information regarding your rights and obligations.
+ *
+ * SOFTWARE AND DOCUMENTATION ARE PROVIDED “AS IS” WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY
+ * OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR
+ * PURPOSE. IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR
+ * OBLIGATED UNDER CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION,
+ * BREACH OF WARRANTY, OR OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT
+ * DAMAGES OR EXPENSES INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL,
+ * INDIRECT, PUNITIVE OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA,
+ * COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY
+ * CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+ * OR OTHER SIMILAR COSTS.
+ *
+ *
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * AUTO-GENERATED CODE:  Graphics Resource Converter version: 3.0.0
+ *****************************************************************************/
+
+/*****************************************************************************
+ * SECTION:  Includes
+ *****************************************************************************/
+#include <Graphics/Graphics.h>
+
+/*****************************************************************************
+ * SECTION:  BITMAPS
+ *****************************************************************************/
+
+/*********************************
+ * TYPE_BITMAP Structure
+ * Label: externalbitmap
+ * Description:  200x160 pixels, 16-bits per pixel
+ ***********************************/
+ /*
+IMAGE_EXTERNAL externalbitmap = 
+{
+    (EXTERNAL | IMAGE_MBITMAP | COMP_NONE),
+    0x0000,
+    0x00000000
+};
+*/
+IMAGE_EXTERNAL Yes = {0x0001,0x0000,0x00000000};
+IMAGE_EXTERNAL Stop_gray = {0x0001,0x0000,0x00004400};
+IMAGE_EXTERNAL Stop = {0x0001,0x0000,0x00008800};
+IMAGE_EXTERNAL Start_gray = {0x0001,0x0000,0x0000CCE8};
+IMAGE_EXTERNAL Start = {0x0001,0x0000,0x000110E8};
+IMAGE_EXTERNAL Standby = {0x0001,0x0000,0x000155D0};
+IMAGE_EXTERNAL Splash = {0x0001,0x0000,0x00054A58};
+IMAGE_EXTERNAL Reset_gray = {0x0001,0x0000,0x00093EE0};
+IMAGE_EXTERNAL Reminder = {0x0001,0x0000,0x000982E0};
+IMAGE_EXTERNAL pumpO = {0x0001,0x0000,0x000D7768};
+IMAGE_EXTERNAL pumpF = {0x0001,0x0000,0x000DBB68};
+IMAGE_EXTERNAL plusP = {0x0001,0x0000,0x000DFF68};
+IMAGE_EXTERNAL ON_gray = {0x0001,0x0000,0x000E320C};
+IMAGE_EXTERNAL OFF_gray = {0x0001,0x0000,0x000E760C};
+IMAGE_EXTERNAL No = {0x0001,0x0000,0x000EBA0C};
+IMAGE_EXTERNAL minusM = {0x0001,0x0000,0x000EFE0C};
+IMAGE_EXTERNAL LED_Mode = {0x0001,0x0000,0x000F30B0};
+IMAGE_EXTERNAL HomeI = {0x0001,0x0000,0x00132538};
+IMAGE_EXTERNAL Home = {0x0001,0x0000,0x001719C0};
+IMAGE_EXTERNAL HF_Mode = {0x0001,0x0000,0x001B0E48};
+IMAGE_EXTERNAL Days = {0x0001,0x0000,0x001F02D0};
+IMAGE_EXTERNAL Code_Entry = {0x0001,0x0000,0x001F7718};
+IMAGE_EXTERNAL Clean_Mode = {0x0001,0x0000,0x00236BA0};
+IMAGE_EXTERNAL Aux_Mode = {0x0001,0x0000,0x00276028};
+/*****************************************************************************
+ * SECTION:  FONTS
+ *****************************************************************************/
+
+/*********************************
+ * TYPE_FONT Structure
+ * Label: externalfont
+ * Description:  Heigth: 31 pixels, range: ' ' to '~'
+ ***********************************/
+FONT_EXTERNAL externalfont = 
+{
+    (EXTERNAL | COMP_NONE),
+    0x0000,
+    0x0000FA10
+};
